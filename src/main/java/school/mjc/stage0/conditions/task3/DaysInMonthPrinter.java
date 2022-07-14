c5321089ccdbdd653cbbffa85ce41cb6b29c16e1
VVZhuchkov <vzhuchkov@tut.by>
2022-07-14 18:31:26 +0300

    Done

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 9 || month == 11) {
+            System.out.println("31");
+        } else if (month == 2) {
+            System.out.println("28");
+        } else if (month <= 12 && month > 0) {
+            System.out.println("30");
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double newSalary=0;
+        if (salary <= 10000 && salary >= 0) {
+            newSalary = salary - 0.15 * salary;
+            System.out.println(newSalary);
+        } else if (salary > 10000 && salary <= 20000) {
+            newSalary = salary - 0.18 * salary;
+            System.out.println(newSalary);
+        } else if (salary > 20000) {
+            newSalary = salary - 0.2 * salary;
+            System.out.println(newSalary);
+        } else if (salary < 0){
+            System.out.println("wrong input!");
+        }
     }
 }
